f7ca496985429dddddc850a5b6721692d7a3aa0f
whatarainyday <409144911@qq.com>
2020-01-16 11:54:33 +0800

    'del'

--- idleitems/src/main/java/com/cat/idleitems/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.cat.idleitems.service;
-
-/**
- * created on 2020/1/11
- */
-public interface UserService {
-}
--- idleitems/src/main/java/com/cat/idleitems/service/imp/UserServiceImp.java
+++
@@ -1,8 +0,0 @@
-package com.cat.idleitems.service.imp;
-
-/**
- * created on 2020/1/11
- */
-public class UserServiceImp {
-
-}
